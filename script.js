c5a6e1c7efca6223f7c65fffb411787d9a0f76cc
Shaarif <1sha2arif2@gmail.com>
2025-04-12 11:11:35 +0530

    commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// JS for basic scroll reveal animation (optional)
+window.addEventListener("scroll", () => {
+    const reveals = document.querySelectorAll("section");
+    for (let i = 0; i < reveals.length; i++) {
+      const windowHeight = window.innerHeight;
+      const revealTop = reveals[i].getBoundingClientRect().top;
+      const revealPoint = 150;
+  
+      if (revealTop < windowHeight - revealPoint) {
+        reveals[i].style.opacity = "1";
+        reveals[i].style.transform = "translateY(0)";
+      }
+    }
+  });
+ 
